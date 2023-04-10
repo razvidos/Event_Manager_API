c6fd68bf5c69e58bbe468409a321293bba87f05d
Ingvar <ingvar.soloma@gmail.com>
2023-04-11 02:13:01 +0300

    UserController swagger docs

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,111 @@ use App\Http\Requests\UpdateUserRequest;
 use App\Http\ResponseStatus;
 use App\Repositories\Interfaces\UserRepositoryInterface;
 use Illuminate\Http\JsonResponse;
+use OpenApi\Annotations as OA;
 
+/**
+ * @OA\Info(
+ *     title="Event Manager API",
+ *     version="1.0.0",
+ *     description="Your API Description",
+ *     @OA\Contact(
+ *         email="ingvar.soloma@gmail.com"
+ *     )
+ * )
+ * @OA\Server(
+ *     url="/api"
+ * )
+ *
+ * @OAS\SecurityScheme(
+ *      securityScheme="bearer_token",
+ *      type="http",
+ *      scheme="bearer"
+ * )
+ *
+ * @OA\Schema(
+ *     schema="User",
+ *     title="User",
+ *     description="User model",
+ *     @OA\Property(property="id", type="integer", description="User ID"),
+ *     @OA\Property(property="name", type="string", description="User name"),
+ *     @OA\Property(property="email", type="string", format="email", description="User email"),
+ *     @OA\Property(property="email_verified_at", type="string", format="date-time",
+ *     description="User verified his email"),
+ *     @OA\Property(property="created_at", type="string", format="date-time", description="User created at timestamp"),
+ *     @OA\Property(property="updated_at", type="string", format="date-time", description="User updated at timestamp")
+ * )
+ *
+ * @OA\Schema(
+ *     schema="UpdateUserRequest",
+ *     title="Update User Request",
+ *     type="object",
+ *     required={"name", "email"},
+ *     properties={
+ *         @OA\Property(property="name", type="string", description="User name"),
+ *         @OA\Property(property="email", type="string", format="email", description="User email"),
+ *     }
+ * )
+ *
+ * @OA\Schema(
+ *     schema="StoreUserRequest",
+ *     title="Store User Request",
+ *     type="object",
+ *     required={
+ *         "name",
+ *         "email",
+ *         "password"
+ *     },
+ *     properties={
+ *         @OA\Property(
+ *             property="name",
+ *             type="string",
+ *             description="Name of the user"
+ *         ),
+ *         @OA\Property(
+ *             property="email",
+ *             type="string",
+ *             format="email",
+ *             description="Email of the user"
+ *         ),
+ *         @OA\Property(
+ *             property="password",
+ *             type="string",
+ *             format="password",
+ *             description="Password of the user"
+ *         ),
+ *         @OA\Property(
+ *             property="phone",
+ *             type="string",
+ *             description="Phone number of the user"
+ *         ),
+ *         @OA\Property(
+ *             property="gender",
+ *             type="string",
+ *             enum={"male", "female", "other"},
+ *             description="Gender of the user"
+ *         ),
+ *         @OA\Property(
+ *             property="dob",
+ *             type="string",
+ *             format="date",
+ *             description="Date of birth of the user"
+ *         ),
+ *     }
+ * )
+ *
+ * @OA\Schema(
+ *     schema="ErrorUserNotFound",
+ *     title="Not Found Error",
+ *     description="The requested resource could not be found but may be available in the future.",
+ *     type="object",
+ *     required={"message"},
+ *     @OA\Property(
+ *         property="message",
+ *         type="string",
+ *         example="User Not Found"
+ *     )
+ * )
+ */
 class UserController extends Controller
 {
 
@@ -19,6 +123,22 @@ class UserController extends Controller
     }
 
     /**
+     * @OA\Get(
+     *     path="/users",
+     *     summary="Get all users",
+     *     tags={"Users"},
+     *     security={{"sunctum":{}}},
+     *     @OA\Response(
+     *         response=200,
+     *         description="OK",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/User")
+     *         )
+     *     ),
+     *
+     * )
+     *
      * @return JsonResponse
      */
     public function index(): JsonResponse
@@ -28,6 +148,37 @@ class UserController extends Controller
     }
 
     /**
+     * @OA\Get(
+     *     path="/users/{id}",
+     *     summary="Get User by ID",
+     *     description="Get User by ID",
+     *     operationId="getUserById",
+     *     tags={"Users"},
+     *     security={{"sunctum":{}}},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="User ID",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int64"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful response",
+     *         @OA\JsonContent(
+     *             ref="#/components/schemas/User"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="Not Found",
+     *         @OA\JsonContent(ref="#/components/schemas/ErrorUserNotFound")
+     *     ),
+     * )
+     *
      * @param int $id
      * @return JsonResponse
      */
@@ -41,6 +192,51 @@ class UserController extends Controller
     }
 
     /**
+     * @OA\Post(
+     *     path="/users",
+     *     summary="Create User",
+     *     description="Create a new User",
+     *     operationId="createUser",
+     *     tags={"Users"},
+     *     security={{"sunctum":{}}},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             ref="#/components/schemas/StoreUserRequest"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="Successful response",
+     *         @OA\JsonContent(
+     *             ref="#/components/schemas/User"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Validation error",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="The given data was invalid"
+     *             ),
+     *             @OA\Property(
+     *                 property="errors",
+     *                 type="object",
+     *                 @OA\Property(
+     *                     property="field_name",
+     *                     type="array",
+     *                     @OA\Items(
+     *                         type="string",
+     *                         example="The field_name field is required."
+     *                     )
+     *                 )
+     *             )
+     *         )
+     *     )
+     * )
      * @param StoreUserRequest $request
      * @return JsonResponse
      */
@@ -52,6 +248,67 @@ class UserController extends Controller
     }
 
     /**
+     * @OA\Put(
+     *     path="/users/{id}",
+     *     summary="Update User",
+     *     description="Update an existing User",
+     *     operationId="updateUser",
+     *     tags={"Users"},
+     *     security={{"sunctum":{}}},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="ID of the User to update",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int64"
+     *         )
+     *     ),
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             ref="#/components/schemas/UpdateUserRequest"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful response",
+     *         @OA\JsonContent(
+     *             ref="#/components/schemas/User"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="Not Found",
+     *         @OA\JsonContent(ref="#/components/schemas/ErrorUserNotFound")
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Validation error",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="The given data was invalid"
+     *             ),
+     *             @OA\Property(
+     *                 property="errors",
+     *                 type="object",
+     *                 @OA\Property(
+     *                     property="field_name",
+     *                     type="array",
+     *                     @OA\Items(
+     *                         type="string",
+     *                         example="The field_name field is required."
+     *                     )
+     *                 )
+     *             )
+     *         )
+     *     )
+     * )
+     *
      * @param UpdateUserRequest $request
      * @param int $id
      * @return JsonResponse
@@ -68,6 +325,34 @@ class UserController extends Controller
     }
 
     /**
+     * @OA\Delete(
+     *     path="/users/{id}",
+     *     summary="Delete User",
+     *     description="Delete a User by ID",
+     *     operationId="deleteUser",
+     *     tags={"Users"},
+     *     security={{"sunctum":{}}},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="ID of the User to delete",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int64"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=204,
+     *         description="No Content"
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="Not Found",
+     *         @OA\JsonContent(ref="#/components/schemas/ErrorUserNotFound")
+     *     ),
+     * )
+     *
      * @param int $id
      * @return JsonResponse
      */

029dac5095c40cc59ceb458c76126ce1065a67f9
Ingvar <ingvar.soloma@gmail.com>
2023-04-11 13:08:38 +0300

    fronted delete

--- resources/views/spa.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div id="app" class="app"></div>
-    <script>
-        window.auth_user = {!! json_encode($auth_user, JSON_THROW_ON_ERROR); !!};
-    </script>
-@endsection
